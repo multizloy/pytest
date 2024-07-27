c9ed563f65792f5395aab7a84508b1a52e74a5e2
Renat Shabanov <multizloy@gmail.com>
2024-07-27 16:17:15 +0300

    test for divide function

--- tests/testMyFunctions.py
+++ tests/testMyFunctions.py
@@ -5,3 +5,8 @@ import source.myFunctions as myFunctions
 def testAdd():
     result = myFunctions.add(1, 4)
     assert result == 5
+
+
+def testDivide():
+    result = myFunctions.divide(10, 5)
+    assert result == 2
